3afea1c0825751919ed72910f30398705fceac40
GabriHR <ghernrod@myuax.com>
2024-05-08 12:34:16 +0200

    .

---
+++ src/main/java/Ejercicio5_Twitter/Email.java
@@ -0,0 +1,13 @@
+package Ejercicio5_Twitter;
+
+public class Email {
+    private String address;
+
+    public Email(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
